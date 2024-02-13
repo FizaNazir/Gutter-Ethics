a2515956a7f3bf0fc2a4a3b72609b08389501586
FizaNazir <159917896+FizaNazir@users.noreply.github.com>
2024-02-13 19:12:58 +0530

    Add files via upload

---
+++ msf.css
@@ -0,0 +1,270 @@
+<style>
+   .text_color{
+  color:#ffffff!important;
+  }
+  #lp-pom-form-248 .option {
+    position: absolute;
+    margin-right: -5px;
+}
+  #lp-code-17{
+  overflow:visible;
+  }
+.optionsList .option {
+    position: unset !important;
+    padding: 5px 5px 5px 0px !important;
+    flex-basis: 49%;
+    height: 23px !important;
+}
+ 
+.option input[type="radio"]{
+    clip: rect(0, 0, 0, 0);
+    height: 1px;
+    width: 1px;
+    border: 0;
+    overflow: hidden;
+}
+  .option input[type="checkbox"] {
+    clip: rect(0, 0, 0, 0);
+    height: 1px;
+    width: 1px;
+    border: 0;
+    overflow: hidden;
+}
+  #lp-pom-form-248 .lp-pom-form-field .lp-form-label{
+      font-family: Montserrat !important;
+      font-weight:600 !important;
+  
+  }
+.optionsList label {
+    
+    display: flex !important;
+    -webkit-transition: all 0.1s ease-in-out;
+    -moz-transition: all 0.1s ease-in-out;
+    -ms-transition: all 0.1s ease-in-out;
+    -o-transition: all 0.1s ease-in-out;
+    transition: all 0.1s ease-in-out;
+    
+    justify-content:center;
+    align-items: center;
+    text-align:center;
+    height: 50px;
+    width: 390px;
+    border: 1px solid #01A127!important;
+    color: #444444;
+    position: relative;  
+}
+.optionsList label:after {
+  
+    font-style: normal;
+    font-variant-caps: normal;
+    font-variant-ligatures: normal;
+    font-weight: 400;
+    text-rendering: auto;
+    font-size: 14px;
+    position: absolute;
+    right: 18px;
+    color: #fff;
+    background: #01A127 !important;
+    -webkit-font-smoothing: antialiased;
+    top: 33%;
+}
+  #lp-pom-form-248 .lp-pom-form-field .opt-label{
+  
+    font-family: Montserrat !important;
+    font-size:16px !important;
+    width:392px;
+    height:52px;
+    background:#FCFDF7;
+    border-radius: 4px;
+ }
+  
+.optionsList label span {
+  
+   color: #444444 !important;
+   text-align: center;
+   font-family: Montserrat !important;
+   font-size: 14px;
+   font-style: normal;
+   font-weight: 400 !important;
+   line-height: 19px;
+   border-radius: 1px;
+  
+}
+  .optionsList .option {
+  margin-bottom: 40px!important;
+  }
+.optionsList label:hover {
+   
+    cursor: pointer;
+    background: #000 !important;
+    color: #fff !important;
+}
+.optionsList label:hover span {
+    color: #FFF !important;
+    background: #000 !important;
+    cursor: pointer;
+}
+.optionsList label:hover:after {
+    color: #FFF ;
+}
+.optionsList input[type="radio"]:checked + label{
+  background: #000!important;
+  color: #FFF  !important;
+
+}
+.optionsList input[type="radio"]:checked + label:after {
+   color: #FFF ;
+}
+.optionsList input[type="radio"]:checked + label span {
+    color: #FFF !important;
+}
+
+.option input[type="radio"] {
+  opacity: 0 !important;
+}
+  .option input[type="radio"]{
+    clip: rect(0, 0, 0, 0);
+    height: 1px;
+    width: 1px;
+    border: 0;
+    overflow: hidden;
+}
+ 
+  div#container_what_service_are_you_looking_for{
+    position: relative;
+    top: 0px;
+}
+  div#container_estimated_square_footage_of_home {
+    position: relative;
+    top: 0px;
+}
+ div#container_street_address {
+       top: 0px;
+}
+  div#container_city {
+        top: 80px;
+}
+  div#container_zip_code {
+        top: 160px;
+}
+  div#container_first_name{
+    top: 0px;
+}
+  div#container_last_name{
+    top: 80px;
+}
+  div#container_phone_number {
+    top: 160px;
+}
+  div#container_email {
+    top: 242px;
+} 
+.progress-bar {
+  border-radius: 3px;
+  background: rgba(251, 188, 5, 0.27) !important;
+  margin-bottom: 20px;
+}
+
+.step-container {
+  display: flex;
+  align-items: center;
+}
+
+.step {
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  height: 30px;
+  background: rgba(251, 188, 5, 0.27);
+  color: #000;
+  font-weight: bold;
+  position: relative;
+  transition: background-color 0.3s ease;
+
+}
+
+.step.active {
+    border-radius: 3px;
+    background: #FFB400;
+}
+
+.step span {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 24px;
+  height: 24px;
+  border-radius: 50%;
+  background: #FBBC05;
+  color: #000;
+  font-weight: bold;
+}
+
+.step.active span {
+    background: #FBBC05;
+    color: #000;
+}
+
+.step:not(.active) {
+  opacity: 0;
+  pointer-events: none;
+}
+
+.step.active ~ .step {
+  opacity: 0;
+}
+  .step{
+  
+  height:10px;}
+  
+    
+.step {
+    width: 16.6%;
+    max-width: 16.6%;
+}
+ 
+  @media only screen and (max-width:767.98px){
+    .optionsList .option {
+    flex-basis: 100%;
+}
+   #lp-pom-form-248 .lp-pom-form-field .opt-label{
+    width:320px;
+    height: 62px; 
+    font-size: 14px !important;
+    }
+   .optionsList label {
+    padding-left: 0px !important;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    height:47px !important;
+  }
+    
+.optionsList .option {
+    padding:5px 1px 5px 0px !important;
+    margin-bottom:39px !important;
+    
+    }
+    
+.optionsList label span {
+  
+    text-align: center;
+    font-family:Montserrat;
+    font-size: 13px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 19px; 
+  }
+
+  
+ label.opt-label.form-builder-radio-label {
+    margin-left: 3px;
+} 
+ #lp-pom-form-248 .lp-pom-form-field .lp-form-label {
+    margin-left: 3px;
+} 
+   
+  }
+ 
+</style>
